55da4fdce27b7ffba9a8d0c863ee2035ffe8c8fc
ChenXinLin <chenxin.lin@mail.citytech.cuny.edu>
2020-11-11 19:15:01 -0500

    In done

---
+++ main.js
@@ -0,0 +1,13 @@
+//alert('Outputing information.');
+console.log('Outputing information.')
+console.log('Outputing information.')
+var number = 1000000;
+var carname = "Ferrari";
+var lol = true;
+var emptycan = null;
+var ssn;
+console.log(number);
+console.log(carname);
+console.log(lol);
+console.log(emptycan);
+console.log(ssn);
