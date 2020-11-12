51dc79f114a5bea9f79be94dea8f5341d48301b5
ChenXinLin <chenxin.lin@mail.citytech.cuny.edu>
2020-11-11 22:56:35 -0500

    Finshed lab

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-//alert('Outputing information.');
-console.log('Outputing information.')
-console.log('Outputing information.')
-var number = 1000000;
-var carname = "Ferrari";
-var lol = true;
-var emptycan = null;
-var ssn;
-console.log(number);
-console.log(carname);
-console.log(lol);
-console.log(emptycan);
-console.log(ssn);
+let a = (4*(13+4)+6*(4+54)-300);
+let b = ((45+45*(-6+34)+4*(12-65))/(1000/300));
+let c = ("This is why I like the way it looks at the tree of LEAF!, ");
+let d = ("Hey, quit looking at me from a far!, ok?");
+const E = (2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274)
+const PI = (3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679)
+console.log(a);
+console.log(b);
+console.log (((a+b)^9999999999999999));
+console.log (c+d);
+console.log(E*(a+b));
+console.log (((PI*E)*(PI*E)*(PI*E))^10000000)
